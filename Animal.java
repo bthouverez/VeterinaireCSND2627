2e9e33fd0809a24628a9079080b996286c405f08
bthouverez <bastien.thouverez@gmail.com>
2026-09-16 09:38:44 +0200

    Intro heritage

---
+++ Animal.java
@@ -0,0 +1,24 @@
+class Animal {
+	protected double poids;
+	protected int taille;
+
+	public Animal() {
+		System.out.println("Constructeur Animal par défaut");
+		this.poids = 5;
+		this.taille = 50;
+	}
+
+	public Animal(double p, int t) {
+		System.out.println("Constructeur Animal paramétré");
+		this.poids = p;
+		this.taille = t;
+	}
+
+	public void faireDuBruit() {
+		System.out.println("Du bruit...");
+	}
+
+	public String toString() {
+		return "Animal qui pèse " + poids + "kg et mesure " + taille + "cm";
+	}
+}
---
+++ PoissonChat.java
@@ -0,0 +1,9 @@
+class PoissonChat extends Animal {
+	private int lg_moustache;
+
+	public PoissonChat() {
+		super();
+		this.lg_moustache = 3;
+		System.out.println("Constructeur PoissonChat par défaut");
+	}
+}
